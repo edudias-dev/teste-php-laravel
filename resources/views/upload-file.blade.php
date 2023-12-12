2ad73a90e604c5bce338d7facef41cb75c6b651e
Eduardo Dias <edu_rodriguesdias@hotmail.com>
2023-12-12 00:03:45 -0300

    minor fixes

--- resources/views/upload-file.blade.php
+++ resources/views/upload-file.blade.php
@@ -32,7 +32,7 @@
     <div class="flex justify-center mt-8">
     <div class="max-w-2xl rounded-lg shadow-xl bg-gray-50">
         <div class="m-4">
-            <label class="inline-block mb-2 text-gray-500">File Upload</label>
+            <label class="inline-block mb-2 text-gray-500">Envio do arquivo</label>
             <div class="flex items-center justify-center w-full">
                 <label
                     class="flex flex-col w-full h-32 border-4 border-blue-200 border-dashed hover:bg-gray-100 hover:border-gray-300">
@@ -43,14 +43,14 @@
                                 d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                         </svg>
                         <p class="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
-                            Attach a file</p>
+                            Clique aqui para selecionar o arquivo</p>
                     </div>
                     <input type="file" name="file" required="required" class="opacity-0" />
                 </label>
             </div>
         </div>
         <div class="flex justify-center p-2">
-            <button class="w-full px-4 py-2 text-white bg-blue-500 rounded shadow-xl">Create</button>
+            <button class="w-full px-4 py-2 text-white bg-blue-500 rounded shadow-xl">Enviar</button>
         </div>
     </div>
 </div>
